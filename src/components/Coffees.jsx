862feb907f9843d5141555e09d5c7a76727bbf31
jordbort <jordan.bortner@gmail.com>
2023-07-09 17:07:40 -0400

    Setup page

---
+++ src/components/Coffees.jsx
@@ -0,0 +1,98 @@
+export default function Coffees() {
+    const coffees = [
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        },
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        },
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        },
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        },
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        },
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        },
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        },
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        },
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        },
+        {
+            img: '',
+            name: '',
+            roast: '',
+            description: '',
+            price: '',
+            rating: '',
+            promo: ''
+        }
+    ]
+    return (
+        <div>
+        </div>
+    )
+}
