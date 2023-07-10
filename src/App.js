0ea55cab5993f313da1080a97c55584fcf2bf7e0
jordbort <jordan.bortner@gmail.com>
2023-07-09 20:10:44 -0400

    Creating coffee component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css'
-import Coffees from './components/Coffees'
+import Coffees from './components/Coffees.jsx'
 
 export default function App() {
   return (
     <div className="App">
-      <p>Welcome</p>
+      <h1>La Colombe coffee roasters</h1>
       <Coffees />
     </div>
   )
